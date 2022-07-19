86737add0e9e1b5d6374d8419d02497fa69183e2
Danilo Barreto Cavalcanti <danilobcaval@gmail.com>
2022-07-19 20:24:09 -0300

    [FEAT] 2psk ported

---
+++ src/python3/2psk.py
@@ -0,0 +1,24 @@
+import sys
+from qam import Qam
+from matplotlib import pyplot as plt
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print(f"Usage: ${sys.argv[0]} <data-bits>")
+        exit(1)
+
+    modulation = { 
+        '0' : (1,0),
+        '1' : (1,180),
+        }
+
+    q = Qam(baud_rate = 10,
+            bits_per_baud = 1,
+            carrier_freq = 50,
+            modulation = modulation)
+    s = q.generate_signal(sys.argv[1])
+
+    plt.figure(1)
+    q.plot_constellation()
+    plt.figure(2)
+    s.plot(dB=False, phase=False, stem=False, frange=(0,500))
